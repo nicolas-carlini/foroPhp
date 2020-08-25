5115a47ddfbc4f3d4ca7ac031e6ded33944f5513
LordComotto <estebanezequielcomotto@gmail.com>
2020-08-25 20:01:53 -0300

    hice andar el axios

--- js/commands.js
+++ js/commands.js
@@ -1,20 +1,29 @@
 'use strict';
 
-const ip="http://35.225.64.241/api/signup.php";
+const url="http://35.225.64.241/api/signin.php";
 
 const pwd=document.getElementById("pwd").value;
 const email=document.getElementById("email").value;
+const enviar=document.getElementById("enviar");
 
-singin() = ()=>axios({
+enviar.onclick= ()=>axios({
     method: 'post',
-    url: ip,
+    url: url,
     data: {
     pwd: pwd,
     email: email
     }
 }).then(Response=>{
     console.log(Response.data)
-    resultado.innerHTML='';
-    resultado.innerHTML+="isRegistered: "+Response.data.isRegistered+" <br>";
-    resultado.innerHTML+="error: "+Response.data.error;
-}).catch(e=>{console.log("Error, La contraseña o el Email estan mal",e)});
+    if(Response.data.error){
+        console.log("error");
+    }
+    else{
+        if(Response.data.isLogged){
+            console.log("esta logeado capo");
+        }
+        else{
+            console.log("hubo un problema al loguearse, intenetelo de nuevo");
+        }
+    }
+}).catch(e=>{console.log("Error",e)});
