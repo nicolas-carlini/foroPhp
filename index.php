fae2e63adf27067757d5b1fd0a76f53b628115f2
LordComotto <estebanezequielcomotto@gmail.com>
2020-08-25 23:02:46 -0300

    hice andar el front del login y signup

--- index.php
+++ index.php
@@ -25,6 +25,6 @@
         </div>
     </main>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.20.0/axios.min.js" integrity="sha512-quHCp3WbBNkwLfYUMd+KwBAgpVukJu5MncuQaWXgCrfgcxCJAq/fo+oqrRKOj+UKEmyMCG3tb8RB63W+EmrOBg==" crossorigin="anonymous"></script>
-    <script src="js/commands.js"></script>
+    <script src="js/signin.js"></script>
 </body>
 </html>
