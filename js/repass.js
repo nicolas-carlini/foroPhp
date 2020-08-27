1e032e8f1f904aa08cf99cac47e83e366afa3de3
LordComotto <estebanezequielcomotto@gmail.com>
2020-08-27 20:58:03 -0300

    repass anda pero tira errores

---
+++ js/repass.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const url = "http://35.225.64.241/api/sendEmail.php";
+
+const email = document.getElementById("email");
+
+
+enviar.onclick = () => {
+
+
+    var formdata = new FormData();
+
+    formdata.append("email", email.value);
+
+    var requestOptions = {
+        headers: {
+            'Content-Type': 'multipart/form-data'
+        }
+    };
+
+    console.log(requestOptions);
+
+    axios.post(url, formdata, requestOptions)
+        .then(Response => {
+            console.log(Response);
+            if (Response.data.error) {
+                console.error("algo ha fallado");
+            } else {
+                if (Response.data.changePassword) {
+                    console.log("se ha enviado un email para cambiar la contra!");
+                } else {
+                    console.log("hubo un problema, intenetelo de nuevo");
+                }
+            }
+        })
+        .catch(e => {
+            console.error("Error", e)
+        });
+
+
+
+}
