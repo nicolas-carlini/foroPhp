e98b741ef2da73b5f1d85df154d0c478261eb2d1
churrasco55 <nikitopedroza2013@gmail.com>
2020-08-28 20:04:58 -0300

    Home y cosas lindas

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
     <main>
         <div class="form">
             
-            <img src="assets/logo1.png" alt="">
+        <img src="assets/logo1.png" class="logardo" alt="si no carga es porque es una bosta la página o tu internet uwu">
 
             <h1 class="wlcm"> Bienvenido </h1>
 
@@ -19,12 +19,12 @@
             <input type="password" name="pwd" id="pwd"class="pwd"placeholder="Contraseña" required>
 
             <button type="submit"class="btn"id="enviar">Ingresar</button>
-            <h1 class="singre">¿No estas registrado? <a href="signup.html">Registrarse</a></h1>
+            <h1 class="singre">¿No estas registrado? <a href="singup.html">Registrarse</a></h1>
             <h1 class="singre"><a href="repass.html">¿Perdiste tu cuenta?</a></h1>
             <p class="cop">© 2020-2021</p>
         </div>
     </main>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.20.0/axios.min.js" integrity="sha512-quHCp3WbBNkwLfYUMd+KwBAgpVukJu5MncuQaWXgCrfgcxCJAq/fo+oqrRKOj+UKEmyMCG3tb8RB63W+EmrOBg==" crossorigin="anonymous"></script>
-    <script src="js/signin.js"></script>
+    <script src="js/commands.js"></script>
 </body>
 </html>
